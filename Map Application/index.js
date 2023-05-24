0328e7c3afe1b7f63e746f76755d4bb21e39bc47
TristanSmith433 <131862500+TristanSmith433@users.noreply.github.com>
2023-05-24 12:37:32 -0400

    Update index.js

--- Map Application/index.js
+++ Map Application/index.js
@@ -1,11 +1,5 @@
 
 
-/*
- StAuth10244: I Tristan Smith, 000825589 
- certify that this material is my original work. No other person's work has been used without due acknowledgement. I have not made my 
- work available to anyone else.
-
-* */
 
 const defaultMarkers = [
   [{
